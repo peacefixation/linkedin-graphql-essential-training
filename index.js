1b7f3b259f9d179d4d9ae0ca0a82f7ff974be3ab
Matt Jarvis <mattjarvis@internode.on.net>
2023-01-30 18:34:03 +1100

    Add productInput mutation

--- index.js
+++ index.js
@@ -8,19 +8,41 @@ app.get('/', (req, res) =>  {
     res.send('GraphQL is amazing!');
 });
 
-const root = { product: () => {
-    return {
-        "id": 2524254,
-        "name": "widget",
-        "description": "Beautiful widget to use in your garden",
-        "price": 34.99,
-        "soldout": false,
-        "stores": [
-            { store: "Pasadena" },
-            { store: "Los Angeles" },
-        ],
-    };
-} };
+class Product {
+    constructor(id, { name, description, price, soldout, stores }) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.soldout = soldout;
+        this.stores = stores;
+    }
+}
+
+const productDatabase = {
+
+};
+
+const root = { 
+    product: () => {
+        return {
+            "id": 2524254,
+            "name": "widget",
+            "description": "Beautiful widget to use in your garden",
+            "price": 34.99,
+            "soldout": false,
+            "stores": [
+                { store: "Pasadena" },
+                { store: "Los Angeles" },
+            ],
+        };
+    },
+    createProduct: ({ input }) => {
+        let id = require('crypto').randomBytes[10].toString('hex');
+        productDatabase[id] = input;
+        return new Product(id, input);
+    }
+};
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
